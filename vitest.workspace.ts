9dd0d096cba65cfd02de8459865078990bcf0223
rainbowkillah <39178890+rainbowkillah@users.noreply.github.com>
2026-01-26 17:42:27 -0500

    feat(M0): Issue #6 - Vitest setup

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,4 +1,15 @@
-export default [
-  '**/vite.config.{mjs,js,ts,mts}',
-  '**/vitest.config.{mjs,js,ts,mts}',
-];
+import { defineWorkspace } from 'vitest/config';
+
+export default defineWorkspace([
+  {
+    test: {
+      environment: 'node',
+      include: ['packages/**/*.test.ts', 'apps/**/*.test.ts'],
+      pool: 'threads',
+      coverage: {
+        provider: 'v8',
+        reportsDirectory: 'coverage'
+      }
+    }
+  }
+]);
